5e6536efd8e87853941663655cdd364e0e4628a9
presleyoreed3 <presleyoreed3@gmail.com>
2022-01-01 13:24:11 -0800

    removed some elements present for testing

---
+++ src/scripts/modal.js
@@ -0,0 +1,29 @@
+class Modal {
+	// Get the modal
+    var modal = document.getElementById("addDrinkModal");
+    // Get the button that opens the modal
+    var btn = document.getElementById("addDrinkModalBtn");
+    
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+    
+    // When the user clicks the button, open the modal 
+    btn.onclick = function() {
+      modal.style.display = "block";
+      raise();
+    }
+    
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
+    
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
+}
+
+module.exports = Modal;
