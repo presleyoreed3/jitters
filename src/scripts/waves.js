6293e8dd003823f00d5024f55c778956e415f2fa
presleyoreed3 <presleyoreed3@gmail.com>
2022-01-01 14:36:47 -0800

    refactored some of my scripts

--- src/scripts/waves.js
+++ src/scripts/waves.js
@@ -1,7 +1,12 @@
-export default function raise() { 
-    e.preventDefault();
-    let waves = document.getElementsByClassName("waves")[0];
-    console.log(waves.height);
-    waves.style.height = "100%"
-    waves.style.maxHeight = "100%"
+class Wave {
+
+    raise() { 
+        let waves = document.getElementsByClassName("waves")[0];
+        waves.style.height = "100%"
+        waves.style.maxHeight = "100%"
+    }
 }
+
+
+
+module.exports = Wave;
