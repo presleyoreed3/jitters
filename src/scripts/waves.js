1fcdb98dc1da5362939c6720bae4359f58c5e15a
presleyoreed3 <presleyoreed3@gmail.com>
2022-01-04 14:43:29 -0800

    added styling for when waves go above the daily max;

--- src/scripts/waves.js
+++ src/scripts/waves.js
@@ -9,6 +9,20 @@ class Wave {
         this.height += percentage;
         waves.style.height = `${this.height}%`
         waves.style.maxHeight = `${this.height}%`
+        this.checkHeight();
+    }
+
+    checkHeight(){
+        let maxHeight = document.getElementsByClassName("mgMax")[0];
+        let maxValue = maxHeight.dataset.mgValue;
+        let currentHeight = document.querySelector("#wave-div");
+        let currentValue = currentHeight.dataset.currentMg;
+        maxValue = parseInt(maxValue);
+        currentValue = parseInt(currentValue);
+        if (currentValue >= maxValue){
+            maxHeight.style.color = "#f2f2f2"
+            maxHeight.style.textShadow = "2px 2px #6A44F2";
+        }
     }
 
 }
