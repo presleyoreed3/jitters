d1c57b9824c7bb84886feb8dd8a15d62c9e47802
presleyoreed3 <presleyoreed3@gmail.com>
2022-01-06 14:52:15 -0800

    comments

--- src/scripts/oz_counter.js
+++ src/scripts/oz_counter.js
@@ -4,6 +4,7 @@ let waveElement = document.querySelector("#wave-div");
 let submitNewDrink = document.querySelector("#add-drink-button")
 drawCounter();
 
+// Function to draw the counter of your current mg count
 function drawCounter() {
 	let amount = 0;
 	let countDisplay = document.createElement("h4");
